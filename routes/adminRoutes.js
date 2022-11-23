22bf795c9e0517d5a424e0733a845a8ac51462a8
youssef115 <58666308+youssef115@users.noreply.github.com>
2022-11-24 00:12:11 +0100

    add udate and get to the admin

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,7 +1,9 @@
 const express=require('express');
 const router=express.Router();
 const Admin=require("../models/adminModel");
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
 //GET ALL ADMINS
 router.get("/",async (req,res)=>{
     try{
@@ -12,9 +14,9 @@ router.get("/",async (req,res)=>{
     }
 })
 //get one admin
-router.get("/getOneAdmin/:login",async (req,res)=>{
+router.get("/getOneAdmin/:id",async (req,res)=>{
     try{
-        let oneAdmin=await Admin.findOne({login:req.params.login})
+        let oneAdmin=await Admin.findOne({_id:req.params.id})
         res.send(oneAdmin);
     }catch(err){
         console.error(err)
@@ -36,5 +38,24 @@ router.post('/addAdmin',async (req,res)=>{
         console.log(err)
     }
 })
+// update admin
+router.put("/updateAdmin/:id",async (req,res)=>{
+    try{
+        bcrypt.hash(req.body.mot_de_passe, saltRounds,async (err,hash)=>{
+        let e=await Admin.findOneAndUpdate({_id:req.params.id},
+            {
+                nom: req.body.nom,
+                prenom: req.body.prenom,
+                login: req.body.login,
+                mot_de_passe: await hash
+            })
+            res.send(e)  
+        })
+             
+    }catch(err){
+        console.log(err)
+    }
+});
+
 
 module.exports=router
